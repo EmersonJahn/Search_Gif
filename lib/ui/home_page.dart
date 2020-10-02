c7a60f03b5067dc66958141e67748c220e5e8e95
Emerson Adierson Jahn <emersonajahn@gmail.com>
2020-10-02 12:55:22 -0300

    Smooth the loading.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gif_finder/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -100,7 +101,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   int _getCount(List data) {
-    if(_search == null) {
+    if(_search == null || _search.isEmpty) {
       return data.length;
     } else {
       return data.length + 1;
@@ -120,7 +121,9 @@ class _HomePageState extends State<HomePage> {
         if (_search == null || index < snapshot.data["data"].length)
           return GestureDetector(
             child: 
-              Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage, 
+                image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
                 height: 300.0,
                 fit: BoxFit.cover,
               ),
